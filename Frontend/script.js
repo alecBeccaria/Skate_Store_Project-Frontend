1c750ff865ffd4c2f9f3681ec1a86a189b0c209a
1DannyGitH <61436532+1DannyGitH@users.noreply.github.com>
2023-02-27 09:50:19 -0700

    Nav bar and Product Objects
    Added Login to Nav Bar on All pages. Made the rest of the product objects In the script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -4,6 +4,66 @@ const product1 = {
     price: '$' + 65.21,
     image: 'img/products/bakerboard.png'
 };
+const product2 = {
+    name: "Powell Peralta Ripper Skateboard Deck",
+    price: '$' +43.00,
+    image: 'img/products/powellboard.png/'
+};
+const product3 = {
+    name: "Real Skateboard Deck",
+    price: '$' + 69.00,
+    image: 'img/products/realboard.png'
+};
+const product4 = {
+    name: "ACE AF1 Skateboard Trucks - Gold (Set of 2)",
+    price: '$' + 62.95,
+    image: 'img/products/Acetrucks.png'
+};
+const product5 = {
+    name: "ACE AF1 Skateboard Trucks - Polished (Set of 2)",
+    price: '$' + 62.95,
+    image: 'img/products/AceTrucks2.png'
+};
+const product6 = {
+    name: "Spitfire Formula Four 52mm Connical Skateboard Wheels",
+    price: '$' + 38.99,
+    image: 'img/products/4wheels.png'
+};
+const product7 = {
+    name: "54mm 84B P5 SPF Sidecut BONES WHEELS Skateboard Wheels- Ripples, White (4pack)",
+    price: '$' + 25.21,
+    image: 'img/products/boneswheels.png'
+};
+const product8 = {
+    name: "54mm 93a Powell Peralta Skateboard Wheels- Dragons (Set of 4)",
+    price: '$' + 54.13,
+    image: 'img/products/dragonwheels.png'
+};
+const product9 = {
+    name: "1″ Enjoi- Colorful Little Buddies Bolts Hardware",
+    price: '$' + 5.95,
+    image: 'img/products/hardware.png'
+};
+const product10 = {
+    name: "Toy Machine X Independent Grip Tape Sheet- Assorted Knuckle Tats",
+    price: '$' + 14.95,
+    image: 'img/products/Griptape1.png'
+};
+const product11 = {
+    name: "10″ Wide Clear Mob Skateboard Grip Tape by the Foot",
+    price: '$' + 3.95,
+    image: 'img/products/Griptape2.png'
+};
+const product12 = {
+    name: "House of Decks Zip-Up Hoodie",
+    price: '$' + 46.49,
+    image: 'img/products/hoodie.png'
+};
+const product13 = {
+    name: "Pig Wheels- Pig Head Skate Wax",
+    price: '$' + 7.95,
+    image: 'img/products/wax.png'
+};
 
 
 function addToCart() {
