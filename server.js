257d2d3e4f445407baaf83d30769b2726b526551
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2023-02-28 12:25:31 -0700

    Changed pages to jade

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const serveIndex = require('serve-index');
+const path = require('path');
 
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.set('view engine', 'jade');
 
 app.use((req, res, next) => {
     console.log('Time: ', Date.now());
@@ -14,12 +19,15 @@ app.use('/request-type', (req, res, next) => {
     next();
 });
 
-app.use('/public', express.static('public'));
-app.use('/public', serveIndex('public'));
 
-
-app.get('/', (req, res) => {
-    res.send('Successful response.');
+app.get('/', function (req, res) {
+    res.render('index', {
+        var1: 'val1',
+        var2: 'val2'
+    });
+});
+app.get('/test', (req, res) => {
+    res.render('test');
 });
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
